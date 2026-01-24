5ecc6a980e393fa0dfdd1fbca408ab4e8a52c6df
justin.chapon <justin.chapon@etu.umontpellier.fr>
2026-01-24 02:47:20 +0100

    SHOP OUI

--- Assets/Scripts/Menu/ShopCloseOpen.cs
+++ Assets/Scripts/Menu/ShopCloseOpen.cs
@@ -3,29 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class ShopCloseOpen : MonoBehaviour
 {
-    private Scene shopScene;
-    private bool isLoaded = false;
-
     public void ToggleShop()
     {
-        Debug.Log("Toggling shop scene");
-        if (!isLoaded)
+        Scene shopScene = SceneManager.GetSceneByName("shop");
+        
+        if (!shopScene.isLoaded)
         {
-            // Première fois : on charge la scène normalement
+            // La scène n'est pas chargée, on la charge
             SceneManager.LoadScene("shop", LoadSceneMode.Additive);
-            isLoaded = true;
         }
         else
         {
-            // Fois suivantes : on alterne juste la visibilité
-            shopScene = SceneManager.GetSceneByName("shop");
-            if (shopScene.isLoaded)
+            // La scène est chargée, on alterne la visibilité
+            foreach (GameObject obj in shopScene.GetRootGameObjects())
             {
-                foreach (GameObject obj in shopScene.GetRootGameObjects())
-                {
-                    // Si c'est affiché, on cache. Si c'est caché, on affiche.
-                    obj.SetActive(!obj.activeSelf);
-                }
+                obj.SetActive(!obj.activeSelf);
             }
         }
     }
--- Assets/Scripts/Menu/shopButtonManager.cs
+++ Assets/Scripts/Menu/shopButtonManager.cs
@@ -7,7 +7,10 @@ public class shopButtonManager : MonoBehaviour
 
     public void SwitchScene()
     {
+        Debug.Log("SwitchScene called");
         Debug.Log("Exiting shop scene");
+        Debug.Log("Attempting to unload 'shop' scene");
         SceneManager.UnloadSceneAsync("shop");
+        Debug.Log("UnloadSceneAsync called for 'shop'");
     }
 }

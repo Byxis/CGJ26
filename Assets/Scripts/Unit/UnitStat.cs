938fff90136caee36860055878eb167cb08f31df
yanis-niaussat <yanis.niaussat@gmail.com>
2026-01-24 03:10:33 +0100

    feat : added sprites animations to scriptable objects

--- Assets/Scripts/Unit/UnitStat.cs
+++ Assets/Scripts/Unit/UnitStat.cs
@@ -47,4 +47,8 @@ public class UnitStats : BaseCard
     [Header("Invocable")]
     public GameObject invocablePrefab;
     public int numberOfInvocable;
+
+    [Header("Animation")]
+    public RuntimeAnimatorController animator;
 }
+
